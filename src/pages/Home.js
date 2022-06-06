ee7b13921abf1126ac6a280546cc02ac1a5fa399
Gabriel <gabriel@sysbase.com.br>
2022-06-06 18:48:25 -0300

    Alteração na versão do app

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,19 @@
 import React from 'react';
-import { Text, View, StatusBar, SafeAreaView } from 'react-native';
+import { Text, View, StatusBar } from 'react-native';
 
 import { styles } from '../assets/css/style';
 
 export default function Home() {
   return (
-    <SafeAreaView style={styles.container}>
-      <View>
-        <StatusBar
-          animated={true}
-          backgroundColor="#ffffff"
-          barStyle={"dark-content"}
-        />
-
-        <Text style={styles.titleHome}>Festas ao redor</Text>
-        <Text style={styles.subtitle}>Encontre o melhor pra você</Text>
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <StatusBar
+        animated={true}
+        backgroundColor="#ffffff"
+        barStyle={"dark-content"}
+      />
+      
+      <Text style={styles.titleHome}>Festas ao redor</Text>
+      <Text style={styles.subtitle}>Encontre o melhor pra você</Text>
+    </View>
   );
 }
