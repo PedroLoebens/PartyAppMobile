9129b805f214172ac92dde486c5773f032e51be7
Pedro Loebens <loebens@outlook.com>
2022-07-14 20:06:05 -0300

    enviando a imagem para o banco em base64

--- Controller.js
+++ Controller.js
@@ -8,7 +8,7 @@ const model=require('./models');
 
 let app=express();
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 //Routes
