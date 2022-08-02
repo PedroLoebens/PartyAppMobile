8e47f8949e5364c70da88170f5c8f4e3d5b15c48
Gabriel <gabriel@sysbase.com.br>
2022-08-02 20:21:01 -0300

    .

--- src/screens/Search.js
+++ src/screens/Search.js
@@ -55,7 +55,7 @@ export default function Search() {
 
           {ResulResearches?.map((search) => (
           <View style={styles.containerSearchEvents}>
-            <Image style={styles.imageEvent} source={require('../assets/images/summer_party.jpg')}/>
+            {/* <Image style={styles.imageEvent} source={require('../assets/images/summer_party.jpg')}/> */}
             <Text style={styles.titleEvent}>{search.name}</Text>
             <Text style={styles.textEvent}><Feather name="map-pin" size={15} />  {search.place}</Text>
             <Text style={styles.textEvent}><Feather name="calendar" size={15} />  {search.date}</Text>
